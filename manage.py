7ecd779b3b1ffe628c5c2df2926741e95eca820b
Paul Greaney <Paul.Greaney@lyit.ie>
2022-02-06 01:16:27 +0000

    Signed-off-by: Paul Greaney <Paul.Greaney@lyit.ie>

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 print("Testing")
-# Comment testing WhiteSource Bolt
+# Comment2 testing WhiteSource Bolt
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
